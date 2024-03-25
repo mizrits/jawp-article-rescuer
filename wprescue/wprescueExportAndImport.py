943586bcd1ce9486cc17301a3e5580b125bd1284
MizRits <109671507+mizrits@users.noreply.github.com>
2024-03-26 01:15:33 +0900

    v1.3 Update wprescueExportAndImport.py

--- wprescue/wprescueExportAndImport.py
+++ wprescue/wprescueExportAndImport.py
@@ -18,7 +18,7 @@ def exportxml(title,fid: str):
     xmlurl=ORIGIN_SCRIPT+"?title=Special:Export&action=submit&history=1&pages="+urllib.parse.quote(title)
     wget.download(xmlurl,fid)
 
-def importxmlandfix(title,fid: str):
+def importxmlandedit(title,fid: str):
     S = requests.Session()
     logintoken = S.get(url=DESTINATION_API, params={"action": "query","meta": "tokens","type": "login","format": "json"}).json()['query']['tokens']['logintoken']
     R = S.post(DESTINATION_API, data={"action":"login","lgname":BOT_NAME,"lgpassword":BOT_PASSWORD,"format":"json","lgtoken":logintoken})
@@ -34,14 +34,14 @@ def importxmlandfix(title,fid: str):
     url=f"{DESTINATION_API}?action=query&format=json&prop=revisions&formatversion=2&rvprop=content&rvslots=main&titles={title}"
     source = requests.get(url).json()['query']['pages'][0]['revisions'][0]['slots']['main']['content']
     edited=re.sub('\<\!\-\-\s削除について[\s\S]*?(.*)しないでください。\s\-\-\>', '{{AutoImported|v1|app=v1|y={{subst:CURRENTYEAR}}|m={{subst:CURRENTMONTH}}|d={{subst:CURRENTDAY2}}}}', source)
-    R = S.post(url=DESTINATION_API, data={"action": "edit","summary": summary,"format": "json","token": csrftoken,"text": edited}).json()
+    R = S.post(url=DESTINATION_API, data={"action": "edit","title": title,"summary": summary,"format": "json","token": csrftoken,"text": edited}).json()
     print(R)
 
 def ExportAndImport(title):
     fid = str(int(time.time()))+".xml" #unique filename
     exportxml(title=title,fid=fid)
-    importxmlandfix(title=title,fid=fid)
+    importxmlandedit(title=title,fid=fid)
 
 
 if __name__ == "__main__":
-    sys.exit(ExportAndImport()) 
+    ExportAndImport(str(input("title")))
