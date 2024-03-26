6e01fe5a8c91f9ac8d7b90c6d669895a2e477c13
MizRits <109671507+mizrits@users.noreply.github.com>
2024-03-26 12:40:00 +0900

    v1.4 encode url

--- wprescue/wprescueExportAndImport.py
+++ wprescue/wprescueExportAndImport.py
@@ -14,24 +14,24 @@ DESTINATION_API = os.environ["DESTINATION_API"]
 BOT_NAME = os.environ["BOT_NAME"]
 BOT_PASSWORD = os.environ["BOT_PASSWORD"]
 
-def exportxml(title,fid: str):
-    xmlurl=ORIGIN_SCRIPT+"?title=Special:Export&action=submit&history=1&pages="+urllib.parse.quote(title)
+def exportxml(titlee,fid: str):
+    xmlurl=f"{ORIGIN_SCRIPT}?title=Special:Export&action=submit&history=1&pages={titlee}"
     wget.download(xmlurl,fid)
 
-def importxmlandedit(title,fid: str):
+def importxmlandedit(title,titlee,fid: str):
     S = requests.Session()
     logintoken = S.get(url=DESTINATION_API, params={"action": "query","meta": "tokens","type": "login","format": "json"}).json()['query']['tokens']['logintoken']
     R = S.post(DESTINATION_API, data={"action":"login","lgname":BOT_NAME,"lgpassword":BOT_PASSWORD,"format":"json","lgtoken":logintoken})
     print(R)
     csrftoken = S.get(url=DESTINATION_API, params={"action": "query","meta": "tokens","format": "json"}).json()['query']['tokens']['csrftoken']
     #### import
-    summary=f"ボットによる自動インポート: ウィキペディア日本語版 https://ja.wikipedia.org/wiki/{title} から全版をインポートしました"
+    summary=f"ボットによる自動インポート: ウィキペディア日本語版 [https://ja.wikipedia.org/wiki/{title}] から全版をインポートしました"
     file={'xml':(fid, open(os.path.abspath(fid)))} #with filepath
     R = S.post(url=DESTINATION_API, files=file, data={"action": "import","summary": summary,"format": "json","token": csrftoken,"interwikiprefix": "jawp"}).json()
     print(R)
-    #### fix
+    #### edit
     summary=f"ボットによる自動編集: インポート後の処理"
-    url=f"{DESTINATION_API}?action=query&format=json&prop=revisions&formatversion=2&rvprop=content&rvslots=main&titles={title}"
+    url=f"{DESTINATION_API}?action=query&format=json&prop=revisions&formatversion=2&rvprop=content&rvslots=main&titles={titlee}"
     source = requests.get(url).json()['query']['pages'][0]['revisions'][0]['slots']['main']['content']
     edited=re.sub('\<\!\-\-\s削除について[\s\S]*?(.*)しないでください。\s\-\-\>', '{{AutoImported|v1|app=v1|y={{subst:CURRENTYEAR}}|m={{subst:CURRENTMONTH}}|d={{subst:CURRENTDAY2}}}}', source)
     R = S.post(url=DESTINATION_API, data={"action": "edit","title": title,"summary": summary,"format": "json","token": csrftoken,"text": edited}).json()
@@ -39,9 +39,9 @@ def importxmlandedit(title,fid: str):
 
 def ExportAndImport(title):
     fid = str(int(time.time()))+".xml" #unique filename
-    exportxml(title=title,fid=fid)
-    importxmlandedit(title=title,fid=fid)
-
+    titlee = urllib.parse.quote(title) #encode
+    exportxml(titlee=titlee,fid=fid)
+    importxmlandedit(title=title,titlee=titlee,fid=fid)
 
 if __name__ == "__main__":
     ExportAndImport(str(input("title")))
